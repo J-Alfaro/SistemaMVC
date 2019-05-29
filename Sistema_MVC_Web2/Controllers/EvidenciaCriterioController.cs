8ff3fab079b668d68b089e2fa9743d74e7228ea3
J-Alfaro <J-Alfaro>
2019-05-29 21:17:23 +0100

    Actualizacion Cesar Calapuja

---
+++ Sistema_MVC_Web2/Controllers/EvidenciaCriterioController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sistema_MVC_Web2.Models;
+using System.IO;
+using Sistema_MVC_Web2.Filters;
+
+namespace Sistema_MVC_Web2.Controllers
+{
+    [Autenticado]
+    public class EvidenciaCriterioController : Controller
+    {
+        //Instanciar las clases
+        private EvidenciaCriterio objEvidenciaCriterio = new EvidenciaCriterio();
+        private Criterio objCriterio = new Criterio();
+
+        //Action Listar
+        public ActionResult Index()
+        {
+            return View(objEvidenciaCriterio.Listar());
+        }
+
+        //Action Visualizar
+
+        public ActionResult Visualizar(int id)
+        {
+            return View(objEvidenciaCriterio.Obtener(id));
+        }
+
+        //Action Agregar Editar
+
+        public ActionResult AgregarEditar(int id = 0)
+        {
+            ViewBag.Criterio = objCriterio.Listar();
+            return View(
+                id == 0 ? new EvidenciaCriterio() // Agregar un nuevo objeto
+                : objEvidenciaCriterio.Obtener(id)
+                );
+        }
+
+
+        //Action Guardar
+        public ActionResult Guardar(EvidenciaCriterio objEvidenciaCriterio, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string archivo = (file.FileName).ToLower();
+
+                    file.SaveAs(Server.MapPath("~/Imagenes/" + file.FileName));
+
+                    objEvidenciaCriterio.archivo= file.FileName;
+                    objEvidenciaCriterio.tamanio = "12";
+                    objEvidenciaCriterio.tipo = "aas";
+                    objEvidenciaCriterio.descripcion = "asdas";
+                }
+                objCriterio.Guardar();
+                return Redirect("~/EvidenciaCriterio");
+            }
+            else
+            {
+                return View("~/Views/EvidenciaCriterio/AgregarEditar.cshtml");
+            }
+        }
+
+        //Action Eliminar
+
+        public ActionResult Eliminar(int id)
+        {
+            objEvidenciaCriterio.evidencia_id = id;
+            objEvidenciaCriterio.Eliminar();
+            return Redirect("~/Usuario");
+        }
+    }
+}
--- Sistema_MVC_Web2/Models/EvidenciaCriterio.cs
+++ Sistema_MVC_Web2/Models/EvidenciaCriterio.cs
@@ -5,6 +5,12 @@ namespace Sistema_MVC_Web2.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
+    using System.Data.Entity;
+
+    using System.Data.Entity.Validation;
+    using System.Web;
+    using System.IO;
 
     [Table("EvidenciaCriterio")]
     public partial class EvidenciaCriterio
@@ -30,5 +36,97 @@ namespace Sistema_MVC_Web2.Models
         public string descripcion { get; set; }
 
         public virtual Criterio Criterio { get; set; }
+        
+
+        //Metodo listar
+        public List<EvidenciaCriterio> Listar()//Retorna una coleccion de registros
+        {
+            var objUsuario = new List<EvidenciaCriterio>();
+            try
+            {
+                using (var db = new Modelo_Sistemas())
+                {
+                    objUsuario = db.EvidenciaCriterio.Include("Criterio").ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objUsuario;
+        }
+
+        //Metodo obtener
+        public EvidenciaCriterio Obtener(int id)//retorna solo un objeto
+        {
+            var objUsuario = new EvidenciaCriterio();
+            try
+            {
+                using (var db = new Modelo_Sistemas())
+                {
+                    objUsuario = db.EvidenciaCriterio.Include("Criterio")
+                        .Where(x => x.evidencia_id == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objUsuario;
+        }
+
+        //Metodo guardar
+        public void Guardar()//retorna solo un objeto
+        {
+
+            try
+            {
+                using (var db = new Modelo_Sistemas())
+                {
+                    if (this.evidencia_id > 0)
+                    {
+                        //si existe un valor mayor a 0 es porque existe un registro
+                        db.Entry(this).State = EntityState.Modified;
+
+                    }
+                    else
+                    {
+                        //si no existe registro graba(nuevo registro)
+                        db.Entry(this).State = EntityState.Added;
+
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        //metodo Eliminar
+        public void Eliminar()
+        {
+
+            try
+            {
+                using (var db = new Modelo_Sistemas())
+                {
+                    db.Entry(this).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+       
+        
+        
     }
 }
