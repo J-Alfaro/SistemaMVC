56ff374fb6c56e8e105906f784f48fe066465409
JhonPeterAguilarAtencio <xxjhonxxaguilarxx@gmail.com>
2019-05-30 12:48:54 -0500

    Control

---
+++ Sistema_MVC_Web2/Controllers/ControlController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Sistema_MVC_Web2.Models;
+
+namespace Sistema_MVC_Web2.Controllers
+{
+    public class ControlController : Controller
+    {
+        private Semestre objSemestre = new Semestre();
+        private Control objControl = new Control();
+        private ControlAsignacion objDetalleControl = new ControlAsignacion();
+
+
+        // GET: Control
+        public ActionResult Index()
+        {
+            return View(objControl.Listar());
+        }
+
+        //Accion visualizar
+        public ActionResult Visualizar(int id)
+        {
+            return View(objControl.Obtener(id));
+        }
+
+        //Acccion agregar editar
+        public ActionResult AgregarEditar(int id = 0)
+        {
+
+            ViewBag.Semestre = objSemestre.Listar();
+
+            return View(
+                id == 0 ? new Control() // Agregar un nuevo objeto
+                : objControl.Obtener(id)
+                );
+        }
+
+        //Accion Guardar
+        public ActionResult Guardar(Control objControl)
+        {
+            if (ModelState.IsValid)
+            {
+
+                objControl.Guardar();
+
+                return Redirect("~/Control");
+            }
+            else
+            {
+                return View("~/Views/Control/AgregarEditar.cshtml");
+            }
+        }
+
+        //accion eleiminar
+        public ActionResult Eliminar(int id)
+        {
+            objControl.control_id = id;
+            objControl.Eliminar();
+            return Redirect("~/Control");
+        }
+
+        public ActionResult DetalleControl(int id)
+        {
+            ViewBag.id = id;
+            return View(objDetalleControl.Listar());
+        }
+    }
+}
--- Sistema_MVC_Web2/Models/Control.cs
+++ Sistema_MVC_Web2/Models/Control.cs
@@ -5,6 +5,7 @@ namespace Sistema_MVC_Web2.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+
     using System.Linq;
     using System.Data.Entity;
 
@@ -28,5 +29,92 @@ namespace Sistema_MVC_Web2.Models
         public string estado { get; set; }
 
         public virtual Semestre Semestre { get; set; }
+
+        //metodo listar
+        public List<Control> Listar()//Retorna una coleccion de registros
+        {
+            var objControl = new List<Control>();
+            try
+            {
+                using (var db = new Modelo_Sistemas())
+                {
+                    objControl = db.Control.Include("Semestre").ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objControl;
+        }
+
+        //metodo obtener
+        public Control Obtener(int id)//retorna solo un objeto
+        {
+            var objControl = new Control();
+            try
+            {
+                using (var db = new Modelo_Sistemas())
+                {
+                    objControl = db.Control.Include("Semestre")
+                        .Where(x => x.control_id == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objControl;
+        }
+
+        //metodo guardar
+        public void Guardar()//retorna solo un objeto
+        {
+
+            try
+            {
+                using (var db = new Modelo_Sistemas())
+                {
+                    if (this.control_id > 0)
+                    {
+                        //si existe un valor mayor a 0 es porque existe un registro
+                        db.Entry(this).State = EntityState.Modified;
+
+                    }
+                    else
+                    {
+                        //si no existe registro graba(nuevo registro)
+                        db.Entry(this).State = EntityState.Added;
+
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        //metodo Eliminar
+        public void Eliminar()
+        {
+
+            try
+            {
+                using (var db = new Modelo_Sistemas())
+                {
+                    db.Entry(this).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
     }
 }
